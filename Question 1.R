54f3cc940ed616ba0c8b7a7838db3b3239fc2f3d
Tilly Morley <matilda.morley@gmail.com>
2024-11-13 13:29:24 +0000

    1

--- Question 1.R
+++ Question 1.R
@@ -1,14 +1,24 @@
 # This analysis models the population growth observed in `Experiment.csv` using a logistic growth model.
 
-### Model Parameters
-N0 <- 879                # Initial population size
-r <- 0.0100086           # Growth rate
-t <- 4980                # Time in minutes
-K <- 5.979e+10           # Carrying capacity for logistic growth
+# Define the logistic growth function
+logistic_fun <- function(t) {
+  # Calculate N based on logistic growth parameters
+  N <- (N0 * K * exp(r * t)) / (K - N0 + N0 * exp(r * t))
+  return(N)
+}
 
-# The logistic growth model is defined by the formula:
-  
-N_logistic <- (N0 * K * exp(r * t)) / (K + (N0 * (exp(r * t) - 1)))
+N0 <- 879 # Initial population size 
+r <- 0.0100086   # Growth rate 
+K <- 5.979e+10   # Carrying capacity 
+
+# Plotting the data and the logistic model with a logarithmic y-axis
+
+ggplot(data = growth_data, aes(x = t, y = N)) +
+  geom_point() +  # Plot observed data points
+  geom_function(fun = logistic_fun, colour = "red") + 
+  xlab("Time (t)") +
+  ylab("Population Size (N)") +
+  theme_bw()
 
 #Results
 #The plot of observed data and the logistic growth model shows that as time progresses, 
