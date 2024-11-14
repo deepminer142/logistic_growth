56f77fdecb6a27f7c3f3d4a2a193c807ef4239fd
Tilly Morley <matilda.morley@gmail.com>
2024-11-14 15:57:38 +0000

    Exam

--- plot_data.R
+++ plot_data.R
@@ -8,17 +8,27 @@ install.packages("ggplot2")
 library(ggplot2)
 
 # Step 3: Plot the data with raw scale for y-axis
-ggplot(data = growth_data, aes(x = t, y = N)) +
+raw_data_plot <- ggplot(data = growth_data, aes(x = t, y = N)) +
   geom_point() +
   xlab("Time (t)") +
   ylab("Population Size (N)") +
   theme_bw()
 
-# Step 4: Plot the data with logarithmic scale on y-axis
-ggplot(data = growth_data, aes(x = t, y = N)) +
+print(raw_data_plot)
+
+# Step 4: Save the plot as PNG
+ggsave("raw_data_plot.png", plot = raw_data_plot, width = 8, height = 6, dpi = 300)
+
+# Step 5: Plot the data with logarithmic scale on y-axis
+raw_data_on_log_plot <- ggplot(data = growth_data, aes(x = t, y = N)) +
   geom_point() +
   xlab("Time (t)") +
   ylab("Population Size (log scale)") +
   scale_y_continuous(trans='log10') +
   theme_bw()
+
+print(raw_data_on_log_plot)
+
+# Step 6: Save the plot as PNG
+ggsave("raw_data_on_log_plot.png", plot = raw_data_on_log_plot, width = 8, height = 6, dpi = 300)
 
